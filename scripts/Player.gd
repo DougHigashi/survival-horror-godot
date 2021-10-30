2e8126e3d620fd210ebac4b6f2bbb453daecdb4a
Douglas Higashi <higoshi.douglas@gmail.com>
2021-10-30 16:31:57 -0300

    Adicionando rotação

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,44 +1,60 @@
 extends KinematicBody
 
-
 export var speed := 6
-export var running_speed := 12
-export var gravity := -30
-export var jump_impulse := 25
+export var rotate_speed := 0.1
+export var gravity := -100
+var rot_dir = 0
 
-var velocity = Vector3.ZERO
+var velocity = Vector3()
 
 onready var pivot := $Pivot
-
-func _ready():
-	var player := $Pivot/MeshInstance
-	player.global_transform.origin
-	
+onready var player := $Player
 
 
 func _physics_process(delta):
-	var input_vector = get_input_Vector()
-	apply_movement(input_vector)
+	rotate_speed = 0.1
+	rotate_player(get_rotation_vector())
+	move_player(get_input_vector())
 	apply_gravity(delta)
-	#velocity = move_and_collide(velocity)
-	velocity = move_and_slide_with_snap(velocity, Vector3.UP, Vector3.UP, true)
+	move_and_slide_with_snap(velocity, Vector3.UP, Vector3.UP, true)
 	
+	
+	var vec = Vector3()
+	var vec2 = Vector3()
+	vec2.z = Input.get_action_strength("move_bw") - Input.get_action_strength("move_fw")
+	vec.y = Input.get_action_strength("rotate_r") - Input.get_action_strength("rotate_l")
+	print(vec)
+	print(vec2)
+	print(rot_dir)
 
-func get_input_Vector():
+func get_rotation_vector():
+	var rotation_vector = Vector3.ZERO
+	rotation_vector.y = Input.get_action_strength("rotate_l") - Input.get_action_strength("rotate_r")
+	if(Input.is_action_pressed("rotate_l")):
+		rot_dir += Input.get_action_strength("rotate_l") - Input.get_action_strength("rotate_r")
+	elif(Input.is_action_pressed("rotate_r")):
+		rot_dir += Input.get_action_strength("rotate_l") - Input.get_action_strength("rotate_r")
+	
+	return rotation_vector
+
+func rotate_player(rotation_vector):
+	if(rotation_vector != Vector3.ZERO):
+		pivot.rotate(rotation_vector, rotate_speed)
+
+func get_input_vector():
 	var input_vector = Vector3.ZERO
-	input_vector.x = Input.get_action_strength("move_r") - Input.get_action_strength("move_l")
-	input_vector.z = Input.get_action_strength("move_bw") - Input.get_action_strength("move_fw")
-
-	return input_vector.normalized()
-
-func apply_movement(input_vector):
-
-	velocity.x = input_vector.x * speed
-	velocity.z = input_vector.z * speed
+	if(Input.get_action_strength("move_bw")):
+		input_vector.z = -1
+	elif(Input.get_action_strength("move_fw")):
+		input_vector.z = 1
+	#rotation_dir = Input.get_action_strength("move_bw") - Input.get_action_strength("move_fw")
+	#input_vector.z = Input.get_action_strength("move_bw") - Input.get_action_strength("move_fw")
 	
-	if(input_vector != Vector3.ZERO):
-		pivot.look_at(translation + input_vector, Vector3.UP)
+	return input_vector
 
+func move_player(input_vector):
+	velocity = input_vector.rotated(Vector3(0, 1, 0), rot_dir * rotate_speed) * speed
 
 func apply_gravity(delta):
 	velocity.y += gravity * delta
+
